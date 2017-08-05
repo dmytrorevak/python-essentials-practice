57075f660d222f325d1b447f9ebaf40944e166be
Dmytro Revak <dmytro.revak@gmail.com>
2017-08-05 14:56:52 +0300

    fix punctuality error

--- calculator.py
+++ calculator.py
@@ -1,26 +1,26 @@
 print("What operation do you want to accomplish ?")
-user_choise = input("+ - * / % \n")
-if user_choise == "+":
+user_choiсe = input("+ - * / % \n")
+if user_choiсe == "+":
     first_addition = float(input("Please enter the first addition "))
     second_addition = float(input("Please enter the second addition "))
     total_sum = str(first_addition + second_addition)
     print("Result is " + total_sum)
-elif user_choise == "-":
+elif user_choice == "-":
     decreasing = float(input("Please enter the decreasing "))
     negative = float(input("Please enter the negative "))
     differance = str(decreasing - negative)
     print("Result is " + differance)
-elif user_choise == "*":
+elif user_choice == "*":
     first_multiplier = float(input("Please enter the first multiplier "))
     second_multiplier = float(input("Please enter the second multiplier "))
     product = str(first_multiplier * second_multiplier)
     print("Result is " + product)
-elif user_choise == "/":
+elif user_choice == "/":
     shared = float(input("Please enter the shared "))
     divider = float(input("Please enter the second divider "))
     fraction = str(shared / divider)
     print("Result is " + fraction)
-elif user_choise == "%":
+elif user_choice == "%":
     shared = float(input("Please enter the shared "))
     divider = float(input("Please enter the second divider "))
     remainder = str(shared % divider)
